d4f7290dcc3acbaaf6d7f993f7eb9d3165e1347c
c76d3656e <71916634+c76d3656e@users.noreply.github.com>
2022-12-18 23:28:12 +0800

    add multiple file load response
    some useless code also be updated

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 use actix_web::{get,web,App,Error, HttpResponse, HttpServer, Responder,web::Data};
 use actix_multipart::Multipart;
 use std::{fs, sync::Arc};
-use std::path::PathBuf;
+
 use std::process::Command;
 use futures::TryStreamExt;
 use uuid::Uuid;
 use std::io::Write;
 use rand::{Rng, thread_rng};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 /// 用于post获得上传文件，上传文件存储在tmp文件夹中
 async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    let mut res:HashSet<String> = HashSet::new();
+    // iterate over multipart stream
+    while let Some(mut field) = payload.try_next().await?
+        let file_num = get_how_many_pictures_in_images().await+1;
+        let filename = file_num.to_string()+".png";
+        let filepath = format!("/images/images/{}", filename);
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath)).await??;
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.try_next().await? {
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&chunk).map(|_| f)).await??;
+        }
+        {
+            let url = "http://setu.works/target/".to_string()+file_num.to_string().as_str();
+            res.insert(url);
+        }
+    }
+    let result = serde_json::to_string(&res).unwrap();
+    Ok(HttpResponse::Ok().body(result))
+}
+
+/// 用于post获得上传文件，上传文件存储在tmp文件夹中
+async fn upload_zip(mut payload: Multipart) -> Result<HttpResponse, Error> {
     // iterate over multipart stream
     while let Some(mut field) = payload.try_next().await? {
         // A multipart/form-data stream has to contain `content_disposition`
@@ -30,13 +56,15 @@ async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
             f = web::block(move || f.write_all(&chunk).map(|_| f)).await??;
         }
     }
-    Ok(HttpResponse::Ok().body("success"))
+    Ok(HttpResponse::Ok().body("success!"))
 }
 
 async fn upload_index(html_base:Data<Arc<HashMap<String, Data<Arc<String>>>>>) -> Result<HttpResponse, Error> {
     let html = html_base.get("upload").unwrap().to_string();
     Ok(HttpResponse::Ok().body(html))
 }
+
+
 
 
 
@@ -67,7 +95,7 @@ async fn get_how_many_pictures_in_images()->i128{
         .trim()
         .parse::<i128>()
         .expect("failed to prase number string to i128");
-    return number-1;
+    return number;
 }
 
 /// # Example
@@ -91,16 +119,10 @@ async fn get_random_picture_id()->String{
 }
 
 
-async fn get_picture_base64(picture_id : &String)->String{
-    // get the file path
-    let mut image_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    image_path.push("images");
-    image_path.push(picture_id.to_owned()+".png");
-    // turn PathBuf to str
-    let image_path = image_path.to_str()
-        .expect("image_path get error");
-    let base64 = image_base64::to_base64(image_path);
-    return base64;
+async fn get_picture_url(picture_id : &String)->String{
+    let picture_id = picture_id.to_owned()+".png";
+    let url = "http://p.c76d.xyz/images/".to_string()+&picture_id;
+    return url;
 }
 
 #[get("/target/{pictureid}")]
@@ -113,29 +135,38 @@ async fn target_img_html(picture_id : web::Path<i64>,html_base:Data<Arc<HashMap<
 
     if (picture_id<=0)||(picture_id>exist_picture_number){
         let sorry = exist_picture_number.to_string()+" is alive,this picture do not exist";
-        HttpResponse::Ok().body(sorry);
+        return HttpResponse::Ok().body(sorry);
     }
     
     let picture_id = picture_id.to_string();
 
-    let base64 = get_picture_base64(&picture_id).await;
+    let url = get_picture_url(&picture_id).await;
     let html_base = html_base.get("random").unwrap();
-    let html = html_replace(html_base, base64, picture_id).await;
+    let html = html_replace(html_base, url, picture_id).await;
     HttpResponse::Ok().body(html)
 }
 
-#[get("/random/")]
-async fn random_img_base64_html(html_base:Data<Arc<HashMap<String, Data<Arc<String>>>>>) -> impl Responder{
+#[get("/random")]
+async fn random_img_html(html_base:Data<Arc<HashMap<String, Data<Arc<String>>>>>) -> impl Responder{
     let picture_id = get_random_picture_id().await;
-    let base64 = get_picture_base64(&picture_id).await;
+    let url = get_picture_url(&picture_id).await;
     let html_base = html_base.get("random").unwrap();
-    let html = html_replace(html_base, base64, picture_id).await;
+    let html = html_replace(html_base, url, picture_id).await;
     HttpResponse::Ok().body(html)
 }
 
-async fn html_replace(html_base:&Data<Arc<String>>,base64:String,picture_id:String)->String{
+#[get("/")]
+async fn random(html_base:Data<Arc<HashMap<String, Data<Arc<String>>>>>) -> impl Responder{
+    let picture_id = get_random_picture_id().await;
+    let url = get_picture_url(&picture_id).await;
+    let html_base = html_base.get("random").unwrap();
+    let html = html_replace(html_base, url, picture_id).await;
+    HttpResponse::Ok().body(html)
+}
+
+async fn html_replace(html_base:&Data<Arc<String>>,url:String,picture_id:String)->String{
     let html_base = html_base.replace("imgname",&(picture_id+".png"));
-    let html = html_base.replace("imgdata", &base64);
+    let html = html_base.replace("imgdata", &url);
     return html;
 }
 
@@ -155,11 +186,18 @@ async fn main() -> std::io::Result<()>{
     HttpServer::new(move ||{
         App::new()
         .app_data(Data::new(Arc::new(html_base.clone())))
-        .service(random_img_base64_html)
+        .service(random_img_html)
+        .service(target_img_html)
+        .service(random)
         .service(
             web::resource("/upload/")
                 .route(web::get().to(upload_index))
                 .route(web::post().to(upload))
+        )
+        .service(
+            web::resource("/uploadzip/")
+                .route(web::get().to(upload_index))
+                .route(web::post().to(upload_zip))
         )
     })
     .workers(16)
